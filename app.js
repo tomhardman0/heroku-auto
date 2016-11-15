5197a2be6c6bcd8a5d993b3ed508242f1d60756c
Thomas Hardman <tomh@moo.com>
2016-11-15 17:36:40 +0000

    fewafe

--- app.js
+++ app.js
@@ -10,35 +10,45 @@ let baseHeaders = {
   'Authorization': `Bearer ${process.env.HEROKU_API}`
 };
 
-
 const options = {
   url: `${process.env.HEROKU_API_BASE}/app-setups`,
   headers: baseHeaders
+};
+
+const handleCreation = (err, res, body, expressRes) => {
+  if (err) throw new Error(err);
+
+  if (body.build && body.build.status === 'succeeded') {
+    expressRes.send(body)
+  } else if (body.status === 'pending')  {
+    options.url = `${process.env.HEROKU_API_BASE}/app-setups/${body.id}`;
+    options.method = 'GET';
+    delete options.body;
+    setTimeout(() => {
+      request(options, (err,res,body) => handleCreation(err,res,body,expressRes));
+    }, 1000);
+  }
 };
 
 app.get('/', function(req, res) {
   res.send('Hello world');
 });
 
-app.get('/doit', function (req, resp) {
+app.get('/doit', function (req, expressRes) {
 
   options['json'] = true;
   options['method'] = 'POST';
   options['body'] = {
     'source_blob': {
-      'url': 'https://api.github.com/repos/tomhardman0/heroku-auto/tarball/master?access_token=${process.env.GIT_ACCESS_TOKEN}',
+      'url': `https://api.github.com/repos/tomhardman0/heroku-auto/tarball/master?access_token=${process.env.GIT_ACCESS_TOKEN}`,
       'checksum': null,
       'version': 1
     }
   };
 
-  request(options, (err, res, body) => {
-    if (err) throw new Error(err);
-    console.log(body);
-    resp.send(body)
-  });
+  request(options, (err,res,body) => handleCreation(err,res,body,expressRes));
 });
 
 app.listen(process.env.PORT || process.env.NODE_PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
