9f93267fe18797d1ff5d8128c5e12b418c8bb9df
Tom Hardman <tomhardman0@gmail.com>
2017-01-25 22:29:57 +0000

    use async in express routes

--- routes/signup.js
+++ routes/signup.js
@@ -1,15 +1,15 @@
 const signUpRoute = (app) => {
     const heroku = app.locals.clients.heroku;
 
-    app.post('/signup', (req, res) => {
+    app.post('/signup', async (req, res) => {
         const envOverrides = {
     		'APP_NAME': 'an app name',
     		'USER_EMAIL': req.body.email,
     		'APP_INIT_PASSWORD': req.body.password
         };
 
-        heroku.signUp(envOverrides)
-        .then((body) => console.log(body));
+        const data = await heroku.signUp(envOverrides);
+        res.send(data);
     });
 
 };
